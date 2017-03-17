01c9d4158d8171ec8d69954a6c379fca09f022ba
Bruce Bowyer-Smyth <bbowyersmyth@live.com.au>
2017-03-17 10:46:27 +1000

    Initial action extension samples

---
+++ ExtensionExamples/ActionExtension/DocumentAction.cs
@@ -0,0 +1,53 @@
+﻿using Intelledox.Extension.Action;
+using Intelledox.QAWizard;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SampleActionExtensions
+{
+    /// <summary>
+    /// This sample demonstrates dealing within generated documents that have been sent into the action.
+    /// </summary>
+    public class DocumentAction : ActionConnector
+    {
+        public override ExtensionIdentity ExtensionIdentity { get; protected set; }
+            = new ExtensionIdentity()
+            {
+                Id = new Guid("D2FC0284-BF5E-41E8-9B05-340378D61710"),
+                Name = "Infiniti Document Action Extension"
+            };
+
+        public async override Task<ActionResult> RunAsync(ActionProperties properties)
+        {
+            ActionResult results = new ActionResult()
+            {
+                Result = Intelledox.QAWizard.Design.ActionResultType.Success
+            };
+
+            try
+            {
+                foreach (var doc in properties.Documents)
+                {
+                    using (var docStream = await properties.GetDocumentStreamAsync(doc))
+                    using (var destStream = new MemoryStream())
+                    {
+                        // Write or send the document to a file, external service, etc
+                        await docStream.CopyToAsync(destStream);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                properties.AddMessage(ex.Message, "Document Action Error");
+                results.Result = Intelledox.QAWizard.Design.ActionResultType.Fail;
+            }
+
+            return results;
+        }
+
+        public override bool SupportsRun() => true;
+
+        public override bool SupportsUI() => false;
+    }
+}
---
+++ ExtensionExamples/ActionExtension/SimpleAction.cs
@@ -0,0 +1,46 @@
+﻿using Intelledox.Extension.Action;
+using Intelledox.QAWizard;
+using System;
+using System.Threading.Tasks;
+
+namespace SampleActionExtensions
+{
+    /// <summary>
+    /// This sample demonstrates the basic shell of an action extension.
+    /// </summary>
+    public class SimpleAction : ActionConnector
+    {
+        // The identity is used for registering the action within Infiniti. The id needs
+        // to be unique and the name is displayed to the user in design.
+        public override ExtensionIdentity ExtensionIdentity { get; protected set; }
+            = new ExtensionIdentity()
+            {
+                Id = new Guid("94773256-9A6C-4281-B2C6-6F854DA076AB"),
+                Name = "Infiniti Simple Action Extension"
+            };
+
+
+        // Main entry point into the action where it will perform its custom operations.
+        // This will be called after the form has been submitted or after each document
+        // generation for a repeating template.
+        public override Task<ActionResult> RunAsync(ActionProperties properties)
+        {
+            ActionResult results = new ActionResult()
+            {
+                Result = Intelledox.QAWizard.Design.ActionResultType.Success
+            };
+
+
+            // Implement custom action details here. Call services, save documents, etc
+
+
+            return Task.FromResult(results);
+        }
+
+        // Whether the action supports the RunAsync method. Typically true.
+        public override bool SupportsRun() => true;
+
+        // Whether the action displays a custom UI after submission. Typically false.
+        public override bool SupportsUI() => false;
+    }
+}
