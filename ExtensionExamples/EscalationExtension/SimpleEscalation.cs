680ff310cbfa205ab94106c48e58a295644bb677
Bruce Bowyer-Smyth <bbowyersmyth@live.com.au>
2017-03-24 12:17:44 +1000

    Initial escalation extension sample

---
+++ ExtensionExamples/EscalationExtension/SimpleEscalation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Intelledox.Extension.Escalation;
+using Intelledox.QAWizard;
+
+namespace SampleEscalationExtension
+{
+    /// <summary>
+    /// This sample demonstrates the basic shell of a escalation extension
+    /// </summary>
+    public class SimpleEscalation : EscalationExtension
+    {
+        public override ExtensionIdentity ExtensionIdentity { get; protected set; }
+            = new ExtensionIdentity()
+            {
+                Id = new Guid("E1B9C3DA-7427-4B53-A235-40A50CE69C73"),
+                Name = "Infiniti Simple Escalation Extension"
+            };
+
+        // Main entry point in the escalation where it will perform its custom operations
+        public override Task RunAsync(EscalationProperties properties)
+        {
+            // Implement custom escalation details here. Send notifications, update services, etc
+
+            return Task.FromResult(0);
+        }
+
+        // Defines whether or not this escalation is allowed run multiple times for a particular project
+        public override bool SupportsRecurring()
+        {
+            return true;
+        }
+    }
+}
